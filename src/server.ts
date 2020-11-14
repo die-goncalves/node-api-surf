e4c322d68a68e109fd7430bc1ac382217a6e45e5
die-goncalves <die.goncalves1990@gmail.com>
2020-11-13 22:24:27 -0300

    carregar o controller no server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import './util/module-alias';
 import { Server } from '@overnightjs/core'
 import bodyParser from 'body-parser';
+import { ForecastController } from './controllers/forecast';
+import { Application } from 'express';
 
 export class SetupServer extends Server {
     constructor(private port = 3000) {
@@ -11,8 +13,17 @@ export class SetupServer extends Server {
     //1° controi o SetupServer e depois chama o init. Não chama no construtor pois teremos operações assincronas.
     public init(): void {
         this.setupExpress();
+        this.setupControllers();
     }
     private setupExpress(): void {
         this.app.use(bodyParser.json());
+    }
+    private setupControllers(): void {
+        const forecastController = new ForecastController
+        //Passar para o overnight que faz o setup no express
+        this.addControllers([forecastController])
+    }
+    public getApp(): Application {
+        return this.app
     }
 }
