535533c8cc9f107d09d86a87daea09a2c71b5691
die-goncalves <die.goncalves1990@gmail.com>
2020-11-16 11:07:13 -0300

    add axios mock

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -1,11 +1,16 @@
-import { StormGlass } from '@src/clients/StormGlass'
+import { StormGlass } from '@src/clients/StormGlass';
+import axios from 'axios';
+
+jest.mock('axios');
 
 describe('StormGlass client', () => {
   it('Should return the normalized forecast from the StormGlass service', async () => {
     const lat = 19.385;
     const lng = -139.4;
 
-    const stormGlass = new StormGlass();
+    axios.get = jest.fn().mockResolvedValue({  });
+
+    const stormGlass = new StormGlass(axios);
     const response = await stormGlass.fetchPoints(lat, lng);
     expect(response).toEqual({})
   })
