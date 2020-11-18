d421f9ef55b5ad5cbe64a861b8cca6a92b47088e
die-goncalves <die.goncalves1990@gmail.com>
2020-11-18 10:45:35 -0300

    usar os métodos connect e close no arquivo do servidor

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import { Server } from '@overnightjs/core';
 import bodyParser from 'body-parser';
 import { ForecastController } from './controllers/forecast';
 import { Application } from 'express';
+import * as database from '@src/database'
 
 export class SetupServer extends Server {
   constructor(private port = 3000) {
@@ -11,9 +12,10 @@ export class SetupServer extends Server {
 
   //Separa a inicialização da aplicação e permitindo que quem inicie o server tenha controle total de como o server será inicializado
   //1° controi o SetupServer e depois chama o init. Não chama no construtor pois teremos operações assincronas.
-  public init(): void {
+  public async init(): Promise<void> {
     this.setupExpress();
     this.setupControllers();
+    await this.databaseSetup();
   }
   private setupExpress(): void {
     this.app.use(bodyParser.json());
@@ -23,6 +25,13 @@ export class SetupServer extends Server {
     //Passar para o overnight que faz o setup no express
     this.addControllers([forecastController]);
   }
+  private async databaseSetup(): Promise<void> {
+    await database.connect();
+  }
+  public async close(): Promise<void> {
+    await database.close();
+  }
+
   public getApp(): Application {
     return this.app;
   }
