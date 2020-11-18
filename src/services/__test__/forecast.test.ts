00b7e6fe53eefc75eaa09feef4ec61f4daf7bab9
die-goncalves <die.goncalves1990@gmail.com>
2020-11-17 22:17:22 -0300

    test fail: should throw internal processing error when something goes wrong during the rating process

--- src/services/__test__/forecast.test.ts
+++ src/services/__test__/forecast.test.ts
@@ -93,4 +93,18 @@ describe('Forecast Service', () => {
     expect(response).toEqual([]);
   });
   
+  it('should throw internal processing error when something goes wrong during the rating process', async () => {
+    const beaches: Beach[] = [
+      {
+        lat: -33.792726,
+        lng: 151.289824,
+        name: 'Manly',
+        position: BeachPosition.E,
+        user: 'some-id',
+      },
+    ];
+    mockedStormGlassService.fetchPoints.mockRejectedValue( 'Error fetching data' );
+    const forecast = new Forecast(mockedStormGlassService);
+    await expect(forecast.processForecastForBeaches(beaches)).rejects.toThrowError(Error);
+  });
 });
