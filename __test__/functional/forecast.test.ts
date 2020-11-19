4771edb034e4979ec729a4c32b69209105849796
die-goncalves <die.goncalves1990@gmail.com>
2020-11-18 22:58:21 -0300

    nome corrigido de forescast para forecast e adicionando uma praia com beforeEach

--- __test__/functional/forecast.test.ts
+++ __test__/functional/forecast.test.ts
@@ -1,4 +1,18 @@
+import { Beach, BeachPosition } from "@src/models/beach";
+
 describe('Beach forecast functional tests', () => {
+  beforeEach(async () => {
+    await Beach.deleteMany({});
+    const defaultBeach = {
+      lat: -33.792726,
+      lng: 151.289824,
+      name: 'Manly',
+      position: BeachPosition.E,
+    };
+    const beach = new Beach(defaultBeach);
+    await beach.save();
+  });
+  
   it('should return a forecast with just a few times', async () => {
     const { body, status } = await global.testRequest.get('/forecast');
     expect(status).toBe(200);
