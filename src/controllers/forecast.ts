923e5e19a40cd50b9f82b52a8483d19b38ce6bb1
die-goncalves <die.goncalves1990@gmail.com>
2020-11-19 10:55:08 -0300

    test passed: add try..catch

--- src/controllers/forecast.ts
+++ src/controllers/forecast.ts
@@ -9,8 +9,12 @@ const forecast = new Forecast();
 export class ForecastController {
   @Get('')
   public async getForecastForgeLoggedUser(_: Request, res: Response): Promise<void> {
-    const beaches = await Beach.find({});
-    const forecastData = await forecast.processForecastForBeaches(beaches);
-    res.status(200).send(forecastData);
+    try{
+      const beaches = await Beach.find({});
+      const forecastData = await forecast.processForecastForBeaches(beaches);
+      res.status(200).send(forecastData);
+    }catch(error){
+      res.status(500).send({ error: 'Something went wrong' });
+    }
   }
 }
