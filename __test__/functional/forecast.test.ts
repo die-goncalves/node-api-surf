cc0d12df7c3533bf92ef6e88d959f4a37fcc122e
die-goncalves <die.goncalves1990@gmail.com>
2020-11-19 10:46:36 -0300

    isolamento da resposta em fixtures

--- __test__/functional/forecast.test.ts
+++ __test__/functional/forecast.test.ts
@@ -1,6 +1,7 @@
 import { Beach, BeachPosition } from "@src/models/beach";
 import nock from 'nock';
 import stormGlassWeather3HoursFixture from "@__test__/fixtures/stormglass_weather_3_hours.json";
+import apiForecastResponse1BeachFixture from '@__test__/fixtures/api_forecast_response_1_beach.json';
 
 describe('Beach forecast functional tests', () => {
   beforeEach(async () => {
@@ -34,67 +35,6 @@ describe('Beach forecast functional tests', () => {
     
     const { body, status } = await global.testRequest.get('/forecast');
     expect(status).toBe(200);
-    expect(body).toEqual([
-      {
-        time: '2020-04-26T00:00:00+00:00',
-        forecast: [
-          {
-            lat: -33.792726,
-            lng: 151.289824,
-            name: 'Manly',
-            position: 'E',
-            rating: 1,
-            swellDirection: 64.26,
-            swellHeight: 0.15,
-            swellPeriod: 3.89,
-            time: '2020-04-26T00:00:00+00:00',
-            waveDirection: 231.38,
-            waveHeight: 0.47,
-            windDirection: 299.45,
-            windSpeed: 100,
-          },
-        ],
-      },
-      {
-        time: '2020-04-26T01:00:00+00:00',
-        forecast: [
-          {
-            lat: -33.792726,
-            lng: 151.289824,
-            name: 'Manly',
-            position: 'E',
-            rating: 1,
-            swellDirection: 123.41,
-            swellHeight: 0.21,
-            swellPeriod: 3.67,
-            time: '2020-04-26T01:00:00+00:00',
-            waveDirection: 232.12,
-            waveHeight: 0.46,
-            windDirection: 310.48,
-            windSpeed: 100,
-          },
-        ],
-      },
-      {
-        time: '2020-04-26T02:00:00+00:00',
-        forecast: [
-          {
-            lat: -33.792726,
-            lng: 151.289824,
-            name: 'Manly',
-            position: 'E',
-            rating: 1,
-            swellDirection: 182.56,
-            swellHeight: 0.28,
-            swellPeriod: 3.44,
-            time: '2020-04-26T02:00:00+00:00',
-            waveDirection: 232.86,
-            waveHeight: 0.46,
-            windDirection: 321.5,
-            windSpeed: 100,
-          },
-        ],
-      },
-    ]);
+    expect(body).toEqual(apiForecastResponse1BeachFixture);
   });
 });
