47e90d45b168e3422bdc23cb5d40a69819fc3874
die-goncalves <die.goncalves1990@gmail.com>
2020-11-19 10:35:38 -0300

    test passed: utilizando nock no teste funcional forecast

--- __test__/functional/forecast.test.ts
+++ __test__/functional/forecast.test.ts
@@ -1,5 +1,6 @@
 import { Beach, BeachPosition } from "@src/models/beach";
 import nock from 'nock';
+import stormGlassWeather3HoursFixture from "@__test__/fixtures/stormglass_weather_3_hours.json";
 
 describe('Beach forecast functional tests', () => {
   beforeEach(async () => {
@@ -15,7 +16,22 @@ describe('Beach forecast functional tests', () => {
   });
   
   it('should return a forecast with just a few times', async () => {
-    nock.recorder.rec();
+    nock('https://api.stormglass.io:443', {
+      encodedQueryParams: true,
+      reqheaders: {
+        Authorization: (): boolean => true,
+      },
+    })
+      .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
+      .get('/v2/weather/point')
+      .query({
+        lat:'-33.792726',
+        lng:'151.289824',
+        params: /(.*)/,
+        source: 'noaa',
+      })
+      .reply(200, stormGlassWeather3HoursFixture);
+    
     const { body, status } = await global.testRequest.get('/forecast');
     expect(status).toBe(200);
     expect(body).toEqual([
@@ -27,7 +43,7 @@ describe('Beach forecast functional tests', () => {
             lng: 151.289824,
             name: 'Manly',
             position: 'E',
-            rating: 2,
+            rating: 1,
             swellDirection: 64.26,
             swellHeight: 0.15,
             swellPeriod: 3.89,
@@ -35,6 +51,7 @@ describe('Beach forecast functional tests', () => {
             waveDirection: 231.38,
             waveHeight: 0.47,
             windDirection: 299.45,
+            windSpeed: 100,
           },
         ],
       },
@@ -46,7 +63,7 @@ describe('Beach forecast functional tests', () => {
             lng: 151.289824,
             name: 'Manly',
             position: 'E',
-            rating: 2,
+            rating: 1,
             swellDirection: 123.41,
             swellHeight: 0.21,
             swellPeriod: 3.67,
@@ -54,6 +71,27 @@ describe('Beach forecast functional tests', () => {
             waveDirection: 232.12,
             waveHeight: 0.46,
             windDirection: 310.48,
+            windSpeed: 100,
+          },
+        ],
+      },
+      {
+        time: '2020-04-26T02:00:00+00:00',
+        forecast: [
+          {
+            lat: -33.792726,
+            lng: 151.289824,
+            name: 'Manly',
+            position: 'E',
+            rating: 1,
+            swellDirection: 182.56,
+            swellHeight: 0.28,
+            swellPeriod: 3.44,
+            time: '2020-04-26T02:00:00+00:00',
+            waveDirection: 232.86,
+            waveHeight: 0.46,
+            windDirection: 321.5,
+            windSpeed: 100,
           },
         ],
       },
