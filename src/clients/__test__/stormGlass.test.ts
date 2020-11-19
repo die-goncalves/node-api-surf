50a07059acc7be7b71f1bb87af51269649b29b77
die-goncalves <die.goncalves1990@gmail.com>
2020-11-19 11:48:37 -0300

    nome corrigido de StormGlass.ts para stormGlass.ts e correção de imports

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -1,4 +1,4 @@
-import { StormGlass } from '@src/clients/StormGlass';
+import { StormGlass } from '@src/clients/stormGlass';
 import * as HTTPUtil from '@src/util/request';
 import StormGlassWeather3HoursFixture from '@__test__/fixtures/stormglass_weather_3_hours.json';
 import StormGlassNormalized3HoursFixture from '@__test__/fixtures/stormglass_normalized_response_3_hours.json';
