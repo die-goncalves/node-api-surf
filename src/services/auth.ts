add82f4708aac0e9fd73d706247da3810c5c1c64
die-goncalves <die.goncalves1990@gmail.com>
2020-11-20 16:07:54 -0300

    Chave secreta e tempo de expiração do JWT em config

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,5 +1,6 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import config from 'config';
 
 export default class AuthService {
   public static async hashPassword( password: string, salt= 10 ): Promise<string> {
@@ -11,6 +12,6 @@ export default class AuthService {
   }
 
   public static generateToken(payload: object): string {
-    return jwt.sign(payload, 'test', { expiresIn: 10000 });
+    return jwt.sign(payload, config.get('App.auth.key'), { expiresIn: config.get('App.auth.tokenExpiresIn') });
   }
 }
