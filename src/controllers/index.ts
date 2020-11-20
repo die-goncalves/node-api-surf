11bc3d7f764adeffe60e43b2e28ac0bde1f31c6c
die-goncalves <die.goncalves1990@gmail.com>
2020-11-19 22:49:35 -0300

    test passed: validação do email no mongoose para lançar ValidatorError em vez de MongoError

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,3 +1,4 @@
+import { CUSTOM_VALIDATION } from '@src/models/user';
 import { Response } from 'express';
 import mongoose from 'mongoose';
 
@@ -5,7 +6,12 @@ export abstract class BaseController {
   //quem extender essa classe vai poder sobreescrever o método
   protected sendCreateUpdateErrorResponse( res: Response, error: mongoose.Error.ValidationError | Error): void {
     if (error instanceof mongoose.Error.ValidationError) {
-      res.status(422).send({ code: 422, error: error.message });
+      const duplicatedKindErrors = Object.values(error.errors).filter((err) => err.kind === CUSTOM_VALIDATION.DUPLICATED);
+      if (duplicatedKindErrors.length){
+        res.status(409).send({ code: 409, error: error.message });
+      } else {
+        res.status(422).send({ code: 422, error: error.message });
+      }
     } else {
       res.status(500).send({ code: 500, error: 'Something went wrong!' });
     }
