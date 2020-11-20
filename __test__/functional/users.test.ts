b7600c3b51ce00e61b4419c0effc25fba3574ebf
die-goncalves <die.goncalves1990@gmail.com>
2020-11-20 15:53:58 -0300

    test fail: Should return UNAUTHORIZED if the user is found but the password does not match

--- __test__/functional/users.test.ts
+++ __test__/functional/users.test.ts
@@ -75,5 +75,19 @@ describe('Users functional tests', () => {
 
       expect(response.status).toBe(401);
     });
+
+    it('Should return UNAUTHORIZED if the user is found but the password does not match', async () => {
+      const newUser = {
+        name: 'John Doe',
+        email: 'john@mail.com',
+        password: '1234',
+      };
+      await new User(newUser).save();
+      const response = await global.testRequest
+        .post('/users/authenticate')
+        .send({ email: newUser.email, password: 'different password' });
+
+      expect(response.status).toBe(401);
+    });
   });
 });
