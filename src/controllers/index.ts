629bf990674750023e25702ece4f799163e82286
die-goncalves <die.goncalves1990@gmail.com>
2020-11-19 21:53:06 -0300

    Criação de classe abstrata para lógica de validação genérica

---
+++ src/controllers/index.ts
@@ -0,0 +1,13 @@
+import { Response } from 'express';
+import mongoose from 'mongoose';
+
+export abstract class BaseController {
+  //quem extender essa classe vai poder sobreescrever o método
+  protected sendCreateUpdateErrorResponse( res: Response, error: mongoose.Error.ValidationError | Error): void {
+    if (error instanceof mongoose.Error.ValidationError) {
+      res.status(422).send({ code: 422, error: error.message });
+    } else {
+      res.status(500).send({ code: 500, error: 'Something went wrong!' });
+    }
+  }
+}
