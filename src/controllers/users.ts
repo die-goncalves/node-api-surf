2cfa73cdd13c625edc53a3d5957f5276d0cefefd
die-goncalves <die.goncalves1990@gmail.com>
2020-11-19 22:03:40 -0300

    Utilização da classe abstrata no controller users

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,9 +1,10 @@
 import { Controller, Post } from '@overnightjs/core';
 import { Response, Request } from 'express';
 import { User } from '@src/models/user';
+import { BaseController } from './index';
 
 @Controller('users')
-export class UsersController {
+export class UsersController extends BaseController{
   @Post('')
   public async create(req: Request, res: Response): Promise<void> {
     try{
@@ -11,7 +12,7 @@ export class UsersController {
       const newUser = await user.save();
       res.status(201).send(newUser);
     }catch(error){
-      res.status(400).send({error: error.message});
+      this.sendCreateUpdateErrorResponse(res, error);
     }
   }
 }
