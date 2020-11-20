1f1f2c10762cb24d0f29e22158f480b9e158fbda
die-goncalves <die.goncalves1990@gmail.com>
2020-11-19 22:23:28 -0300

    test fail: should return 409 when the email already exists

--- __test__/functional/users.test.ts
+++ __test__/functional/users.test.ts
@@ -29,5 +29,21 @@ describe('Users functional tests', () => {
         error: 'User validation failed: name: Path `name` is required.',
       });
     });
+
+    it('should return 409 when the email already exists', async () => {
+      const newUser = {
+        name: 'John Doe',
+        email: 'john@mail.com',
+        password: '1234',
+      };
+      await global.testRequest.post('/users').send(newUser);
+      const response = await global.testRequest.post('/users').send(newUser);
+
+      expect(response.status).toBe(409);
+      expect(response.body).toEqual({
+        code: 409,
+        error: 'User validation failed: email: already exists in the database.',
+      });
+    });
   });
 });
