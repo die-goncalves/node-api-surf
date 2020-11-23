07278185aa3491324833e7f2ed18ae4e9cc163ff
die-goncalves <die.goncalves1990@gmail.com>
2020-11-23 14:58:04 -0300

    add function decodeToken in auth service

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,6 +1,11 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import config from 'config';
+import { User } from '@src/models/user';
+
+export interface DecodedUser extends Omit<User, '_id'> {
+  id: string;
+}
 
 export default class AuthService {
   public static async hashPassword( password: string, salt= 10 ): Promise<string> {
@@ -13,5 +18,9 @@ export default class AuthService {
 
   public static generateToken(payload: object): string {
     return jwt.sign(payload, config.get('App.auth.key'), { expiresIn: config.get('App.auth.tokenExpiresIn') });
+  }
+
+  public static decodeToken(token: string): DecodedUser {
+    return jwt.verify(token, config.get('App.auth.key')) as DecodedUser;
   }
 }
