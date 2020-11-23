f9076791548becdade5dda3528dd55d908c4a1d8
die-goncalves <die.goncalves1990@gmail.com>
2020-11-23 14:33:34 -0300

    test fail: should verify a JWT token and call the next middleware

---
+++ src/middlewares/__test__/auth.test.ts
@@ -0,0 +1,16 @@
+import AuthService from "@src/services/auth";
+
+describe('AuthMiddleware', () => {
+  it('should verify a JWT token and call the next middleware', () => {
+    const jwtToken = AuthService.generateToken({ data: 'fake' });
+    const reqFake = {
+      headers: {
+        'x-access-token': jwtToken,
+      },
+    };
+    const resFake = {};
+    const nextFake = jest.fn();
+    authMiddleware(reqFake, resFake, nextFake);
+    expect(nextFake).toHaveBeenCalled();
+  });
+});
