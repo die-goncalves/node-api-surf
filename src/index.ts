18a3d1d5600a2ba871330437c2a3f3447e10b165
die-goncalves <die.goncalves1990@gmail.com>
2020-11-25 15:03:12 -0300

    add graceful shutdown

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,32 @@
 import { SetupServer } from './server';
 import config from 'config';
+import logger from './logger';
+
+enum ExitStatus {
+  Failure = 1,
+  Success = 0,
+}
 
 (async (): Promise<void> => {
-  const server = new SetupServer(config.get('App.port'));
-  await server.init();
-  server.start();
+  try {
+    const server = new SetupServer(config.get('App.port'));
+    await server.init();
+    server.start();
+
+    const exitSignals: NodeJS.Signals[] = ['SIGINT', 'SIGTERM', 'SIGQUIT'];
+    exitSignals.map((sig) => process.on(sig, async () => {
+      try {
+        await server.close();
+        logger.info(`App exited with success`);
+        process.exit(ExitStatus.Success);
+      } catch (error) {
+        logger.error(`App exited with error: ${error}`);
+        process.exit(ExitStatus.Failure);
+      }
+    })
+    );
+  } catch (error) {
+    logger.error(`App exited with error: ${error}`)
+    process.exit(ExitStatus.Failure);
+  }
 })();
