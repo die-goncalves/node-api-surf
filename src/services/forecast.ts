eb6da3f4c51b78713c4db3f7737ae8fa323fd01f
die-goncalves <die.goncalves1990@gmail.com>
2020-11-27 15:02:17 -0300

    refactor: criação do método calculateRating

--- src/services/forecast.ts
+++ src/services/forecast.ts
@@ -22,16 +22,9 @@ export class Forecast {
   constructor(protected stormGlass = new StormGlass(), protected RatingService: typeof Rating = Rating) { }
 
   public async processForecastForBeaches(beaches: Beach[]): Promise<TimeForecast[]> {
-    const pointsWithCorrectSources: BeachForecast[] = [];
-    logger.info(`Preparing the forecast for ${beaches.length} beaches`);
     try {
-      for (const beach of beaches) {
-        const rating = new this.RatingService(beach);
-        const points = await this.stormGlass.fetchPoints(beach.lat, beach.lng);
-        const enrichedBeachData = this.enrichBeachData(points, beach, rating);
-        pointsWithCorrectSources.push(...enrichedBeachData);
-      }
-      const timeForecast = this.mapForecastByTime(pointsWithCorrectSources);
+      const beachForecast = await this.calculateRating(beaches);
+      const timeForecast = this.mapForecastByTime(beachForecast);
       return timeForecast.map((t) => ({
         time: t.time,
         forecast: _.orderBy(t.forecast, ['rating'], ['desc']),
@@ -40,6 +33,18 @@ export class Forecast {
       logger.error(error);
       throw new ForecastProcessingInternalError(error.message);
     }
+  }
+
+  private async calculateRating(beaches: Beach[]): Promise<BeachForecast[]> {
+    const pointsWithCorrectSources: BeachForecast[] = [];
+    logger.info(`Preparing the forecast for ${beaches.length} beaches`);
+    for (const beach of beaches) {
+      const rating = new this.RatingService(beach);
+      const points = await this.stormGlass.fetchPoints(beach.lat, beach.lng);
+      const enrichedBeachData = this.enrichBeachData(points, beach, rating);
+      pointsWithCorrectSources.push(...enrichedBeachData);
+    }
+    return pointsWithCorrectSources;
   }
 
   private enrichBeachData(points: ForecastPoint[], beach: Beach, rating: Rating): BeachForecast[] {
