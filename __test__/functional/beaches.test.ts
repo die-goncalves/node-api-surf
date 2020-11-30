b53200b66cbb34404ab630773b4a92341ac36058
die-goncalves <die.goncalves1990@gmail.com>
2020-11-30 10:50:15 -0300

    mudar mensagem de erro para bater com a documentação

--- __test__/functional/beaches.test.ts
+++ __test__/functional/beaches.test.ts
@@ -45,7 +45,7 @@ describe('Beaches functional tests', () => {
         code: 400,
         error: 'Bad Request',
         message:
-          'Beach validation failed: lat: Cast to Number failed for value "invalid_string" at path "lat"',
+          'request.body.lat should be number',
       });
     });
 
