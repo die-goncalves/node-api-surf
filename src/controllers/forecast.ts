da52e3f08a37b36e7513aca59422eac8ffb4b50a
die-goncalves <die.goncalves1990@gmail.com>
2020-11-30 14:38:48 -0300

    add middleware ratelimit

--- src/controllers/forecast.ts
+++ src/controllers/forecast.ts
@@ -1,17 +1,36 @@
-import { ClassMiddleware, Controller, Get } from '@overnightjs/core';
+import { ClassMiddleware, Controller, Get, Middleware } from '@overnightjs/core';
 import logger from '@src/logger';
 import { authMiddleware } from '@src/middlewares/auth';
 import { Beach } from '@src/models/beach';
 import { Forecast } from '@src/services/forecast';
+import ApiError from '@src/util/errors/api-error';
 import { Request, Response } from 'express';
+import rateLimit from 'express-rate-limit';
 import { BaseController } from '.';
 
 const forecast = new Forecast();
+
+const rateLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute in milliseconds
+  max: 10, // 10 requests por minuto. Limitar por IP
+  keyGenerator(req: Request): string {
+    return req.ip;
+  },
+  handler(_, res: Response): void {
+    res.status(429).send(
+      ApiError.format({
+        code: 429,
+        message: "Too many requests to the '/forecast endpoint'",
+      })
+    );
+  },
+});
 
 @Controller('forecast') //Automaticamente teremos uma rota /forecast
 @ClassMiddleware(authMiddleware)
 export class ForecastController extends BaseController {
   @Get('')
+  @Middleware(rateLimiter)
   public async getForecastForgeLoggedUser(req: Request, res: Response): Promise<void> {
     try {
       const beaches = await Beach.find({ user: req.decoded?.id });
